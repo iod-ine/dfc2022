e82aaeb4d84014f641b71c58d2f464182d7d5ebc
Ivan Dubrovin <Ivan.Dubrovin@skoltech.ru>
2022-01-29 19:26:05 +0300

    Change encoder tests to use NTXent loss
    Replaced MSE loss in tests for FC-DenseNet103 encoder to NTXent.

--- tests/models/test_fcdensenet103_enc.py
+++ tests/models/test_fcdensenet103_enc.py
@@ -6,6 +6,7 @@ import torch.nn.functional
 import torch.backends.cudnn
 
 from hashtagdeep.models import FCDenseNet103Encoder
+from hashtagdeep.losses import NTXentLoss
 
 
 @pytest.fixture(scope='function')
@@ -14,16 +15,21 @@ def model():
     return model
 
 
-def test_parameters_change_after_learning_step(model):
+@pytest.fixture(scope='module')
+def ntxent():
+    return NTXentLoss(temperature=0.5)
+
+
+def test_parameters_change_after_learning_step(model, ntxent):
     """ If the parameters are not explicitly frozen, they should change after one learning step. """
 
     optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 
     params_before = [(name, data.detach().clone()) for name, data in model.named_parameters()]
 
-    xx = torch.ones(1, 4, 256, 256)
+    xx = torch.ones(4, 4, 256, 256)
     yy = model(xx)
-    loss = torch.nn.functional.mse_loss(yy, torch.ones(1, 128))
+    loss = ntxent(yy)
     loss.backward()
     optimizer.step()
 
@@ -33,25 +39,25 @@ def test_parameters_change_after_learning_step(model):
         assert torch.any(before[1] != after), f'Parameter {before[0]} did not change'
 
 
-def test_loss_decreases_after_learning_step(model):
+def test_loss_decreases_after_learning_step(model, ntxent):
     """ The loss should decrease after each learning step. """
 
     optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 
     xx = torch.ones(1, 4, 256, 256)
     yy = model(xx)
-    loss0 = torch.nn.functional.mse_loss(yy, torch.ones(1, 128))
+    loss0 = ntxent(yy)
     loss0.backward()
     optimizer.step()
 
     yy = model(xx)
-    loss1 = torch.nn.functional.mse_loss(yy, torch.ones(1, 128))
+    loss1 = ntxent(yy)
 
     assert loss1.item() <= loss0.item(), 'Loss increased after a learning step'
 
 
 @torch.no_grad()
-def test_loss_is_not_zero(model):
+def test_loss_is_not_zero(model, ntxent):
     """ The loss should never be zero. """
 
     xx = torch.ones(1, 4, 256, 256)
